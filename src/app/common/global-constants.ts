f1a55b62f125107c9c230c37c890a9af0a38d53a
“Yashar <79.zand@gmail.com>
2020-07-13 16:54:55 +0800

    backend on heroku now

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,5 +1,6 @@
 export class GlobalConstants {
-    public static apiURL: string = "http://localhost:3000";
+    // public static apiURL: string = "http://localhost:3000";
+    public static apiURL: string = "https://ciacc-backend.herokuapp.com";
       
     public static username: string = "";
 }
