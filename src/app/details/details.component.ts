32b358937bcc6e8fa55b9db76cf30d08164a525c
“Yashar <79.zand@gmail.com>
2020-07-14 18:43:57 +0800

    fix date bug

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -37,26 +37,29 @@ export class DetailsComponent implements OnInit {
     this.bookingService.getBookings('/bookings/user/' + query).subscribe(data => {
       
       this.bookings = data.map(booking => {
-        let ds = new Date(booking.date)
-        let de = new Date(ds.valueOf() + (30 * 60 * 1000))
+        let ds = new Date(booking.date);
+        let de = new Date(ds.valueOf() + (30 * 60 * 1000));
+        console.log(ds);
+        console.log(ds.toISOString());
         let st = this.formater(ds);
         let et = this.formater(de);
         booking["calendar"] = "https://calendar.google.com/calendar/r/eventedit?dates="
           + st + "/" + et + "&text=Carsome+Inspection"
           + "&location=No.+28G,+Jalan+Bandar+Tiga,+Pusat+Bandar+Puchong,+47610+Puchong,+Selangor&details=Please+remember+to+bring+your+documents.&sf=true";
-        // console.log(booking["calendar"]);
+        console.log(booking["calendar"]);
         return booking;
       });
     })
   }
 
   formater = (dt : Date) => {
-    const formatter = new Intl.DateTimeFormat('my',{
-      year: 'numeric', month: '2-digit', day: '2-digit',
-      hour: '2-digit', minute: '2-digit', second: '2-digit'
-    });
-    var parts = formatter.formatToParts(dt);
-    return parts[4].value+parts[0].value+parts[2].value+"T"+parts[6].value+parts[8].value+parts[10].value+"Z";
+    // const formatter = new Intl.DateTimeFormat('my',{
+    //   year: 'numeric', month: '2-digit', day: '2-digit',
+    //   hour: '2-digit', minute: '2-digit', second: '2-digit'
+    // });
+    // var parts = formatter.formatToParts(dt);
+    // return parts[4].value+parts[0].value+parts[2].value+"T"+parts[6].value+parts[8].value+parts[10].value+"Z";
+    return dt.toUTCString().replace(/-/g,'').replace(/:/g,'').split(".")[0]+"Z";
   }
 
 }
